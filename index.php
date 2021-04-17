6e8c398a7248a7e2937ad9524f0798fea15bc7c4
mhmmadeffan <82354518+mhmmadeffan@users.noreply.github.com>
2021-04-17 10:35:01 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Youtube Player</title>
+</head>
+<body>
+<iframe style="width: 450px;height: 340px;"type="text/html" src="https://youtu.be/X8mxrHZblBU" frameborder="0"></iframe>	
+</body>
+</html>
